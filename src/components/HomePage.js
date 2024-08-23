08149eac5c397b4a99e3847b1febb0e6c89b0829
Leonardo Bitencourt Marques <leonardo.marques@dbserver.com.br>
2024-08-23 16:13:04 -0300

    amostrando pro pessoal a trigger

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,6 +7,7 @@ const HomePage = () => {
       <div className="home-box">
         <h1>Hello World</h1>
         <h2>Github + jenkins hook</h2>
+        <h2>trigger????</h2>
       </div>
     </div>
   );
